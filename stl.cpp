85823547963a74ae388b088c8517e272f50fce37
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-12 23:13:19 +0530

    add copy definition and snytax

--- stl.cpp
+++ stl.cpp
@@ -75,6 +75,10 @@ void explainVector()
   vector<int> v(2, 100);
   v.insert(v.begin(), 300);       //  {300, 100, 100}
   v.insert(v.begin() + 1, 2, 10); //  {300, 10, 10, 100, 100}
+
+  //  Copy a vector
+  vector<int> copy(2, 50);                       //  {50, 50}
+  v.insert(v.begin(), copy.begin(), copy.end()); //  {50, 50, 300, 10, 10, 100, 100}
 }
 
 int main()
