8c2d72e6afcddc7ac170abfaecd5c1bf7b77eef3
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-14 10:45:07 +0530

    add list defintion, syntax and functions

--- stl.cpp
+++ stl.cpp
@@ -95,6 +95,21 @@ void explainVector()
   cout << v.empty(); // outputs "true" if the vector is empty and "false" if it's not empty
 }
 
+void explainList()
+{
+  // Similar to vectos, provied front operations as well
+  list<int> ls;
+
+  ls.push_back(2);    //  {2}
+  ls.emplace_back(4); //  {2, 4}
+
+  ls.push_front(5);   //  {5, 2, 4}
+  ls.emplace_front(); //  {2, 4}
+
+  // rest fubnctions are same as vector
+  // begin, end, rbegin, rend, clear, insert, size, swap
+}
+
 int main()
 {
   pairsExplained();
