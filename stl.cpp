54650ee1facd2039197be6e3a625c119aec5d955
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-14 13:22:11 +0530

    add priority queue definition, syntax and fucntions

--- stl.cpp
+++ stl.cpp
@@ -152,6 +152,8 @@ void exolainStack()
 
 void explainQueue()
 {
+  // FIFO(First In Frist Out)
+
   queue<int> q;
   q.push(1);    //  {1}
   q.push(2);    //  {1, 2}
@@ -164,6 +166,34 @@ void explainQueue()
   q.pop();           // {2, 9}
 
   cout << q.front(); //  prints 2
+}
+
+void explainPQ()
+{
+  priority_queue<int> pq; //  using this declaration puts the largest element at the top
+
+  pq.push(5);     //  {5}
+  pq.push(2);     //  {5, 2}
+  pq.push(8);     //  {8, 5, 2}; 8 > 5 so it goes to the top
+  pq.emplace(10); //  {10, 8, 5, 2}
+
+  cout << pq.top(); //  prints 10
+
+  pq.pop();         //  {8, 5, 2}
+  cout << pq.top(); //  prints 8
+
+  // size, swap, empty functions are same as others
+
+  // Minimum Heap
+  // to keep the smallest element at the top
+
+  priority_queue<int, vector<int>, greater<int>> pq1;
+  pq1.push(5);     //  {5}
+  pq1.push(2);     // {2, 5}
+  pq1.push(8);     //  {2, 5, 8}
+  pq1.emplace(10); //  {2, 5, 8, 10}
+
+  pq1.top(); //  prints 2(smallest of the lot)
 }
 
 int main()
