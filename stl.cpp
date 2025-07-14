87513c910861ef829294c9c19bab363cb050142a
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2025-07-14 10:58:41 +0530

    add stack syntax, definition, functions

--- stl.cpp
+++ stl.cpp
@@ -128,6 +128,28 @@ void explainDeque()
   // begin, end, rbegin, rend, clear, insert, size, swap
 }
 
+void exolainStack()
+{
+  // LIFO(Last In First Out)
+
+  stack<int> st;
+  st.push(1);    // {1}
+  st.push(2);    // {2, 1}
+  st.push(3);    // {3, 2, 1}
+  st.push(4);    // {4, 3, 2, 1}
+  st.emplace(5); // {5, 4, 3, 2, 1}
+
+  cout << st.top(); // prints 5 "** st[2] is invalid **"
+
+  st.pop();          //  st looks like {4, 3, 2, 1}
+  cout << st.top();  //  4
+  cout << st.size(); //  4
+  cout << st.empty();
+
+  stack<int> st1, st2;
+  st1.swap(st2);
+}
+
 int main()
 {
   pairsExplained();
